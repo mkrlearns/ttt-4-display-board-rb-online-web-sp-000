bcd13702ad6ba5534a6bb9665245f367a8b4e3c1
ryan m <contact@ryanmeek.com>
2019-01-23 20:33:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
+  puts " #{0} | #{1} | #{2}"
+  puts " #{3} | #{0} | #{0}"
   puts " #{0} | #{0} | #{0}"
 end
